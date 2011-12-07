77a5a126fcf64c194445ba301f05ffc34662c72f
Emanuil Tolev <emanuil.tolev@gmail.com>
2011-12-07 19:42:08 +0000

    Starting on the first version of the twitter class

--- whatid/tweetlisten.py
+++ whatid/tweetlisten.py
@@ -6,8 +6,9 @@ import twitter
 class TweetListen(object):
     def __init__(self):
         pass
+        #api = twitter.Api(consumer_key='consumer_key', consumer_secret='consumer_secret', access_token_key='access_token', access_token_secret='access_token_secret')
         
 
-https://api.twitter.com/1/statuses/mentions.json?include_entities=true
+#https://api.twitter.com/1/statuses/mentions.json?include_entities=true
 
 #https://dev.twitter.com/docs/auth/oauth
