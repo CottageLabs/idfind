ae90b685f230b081dd17372ad80fc778859d3933
Emanuil Tolev <emanuil.tolev@gmail.com>
2012-01-04 23:05:57 +0000

    save current state of work - cont. in lunch break

--- whatid/tweetlisten.py
+++ whatid/tweetlisten.py
@@ -2,13 +2,20 @@
 it tries to identify it and passes back the result as a tweet'''
 
 import twitter
+import json
 import whatid.dao
+
+#credentials = whatid.dao.TwitterCredentials.query(q='*')
+#print credentials['hits']['hits'][0]['_source']
+fp = open('twittercreds.json', 'r')
+print fp
+#json.load(fp)
 
 class TweetListen(object):
     def __init__(self):
         # EET: need to learn how to query ES once and for all from code - just index/type and also  index/type/name:value pairs. Get the twitter credentials from the ES index and use them to connect, then continue development: we need to parse mentions (and perhaps DMs?) as per issue #1 on GitHub
-        #credentials = whatid.dao.TwitterCredentials.query
-        
+        credentials = whatid.dao.TwitterCredentials.query(q='_search?q=*')
+        print credentials
         #api = twitter.Api(consumer_key='consumer_key', consumer_secret='consumer_secret', access_token_key='access_token', access_token_secret='access_token_secret')
         
 
