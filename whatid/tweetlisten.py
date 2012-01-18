dd468da1a9dd7bba75edc00f55cad1a48b0b3890
Emanuil Tolev <emanuil.tolev@gmail.com>
2012-01-18 23:52:50 +0000

    more progress - authentication successful and verified for idhelp account

--- whatid/tweetlisten.py
+++ whatid/tweetlisten.py
@@ -1,6 +1,7 @@
 '''this tracks tweets on twitter and when one comes in with a string to identify,
 it tries to identify it and passes back the result as a tweet'''
 
+import urllib2
 import twitter
 import json
 import whatid.dao
@@ -10,10 +11,18 @@ class TweetListen(object):
         # EET: need to learn how to query ES once and for all from code - just index/type and also  index/type/name:value pairs. Get the twitter credentials from the ES index and use them to connect, then continue development: we need to parse mentions (and perhaps DMs?) as per issue #1 on GitHub
         credentials = whatid.dao.TwitterCredentials.query(q='*')
         # OK, that works - we can now create the twitter.Api below
-        print credentials['hits']['hits'][0]['_source']['consumer_key']
+        # print credentials['hits']['hits'][0]['_source']['consumer_key']
         
-        #api = twitter.Api(consumer_key='consumer_key', consumer_secret='consumer_secret', access_token_key='access_token', access_token_secret='access_token_secret')
+        api = twitter.Api(
+            consumer_key = credentials['hits']['hits'][0]['_source']['consumer_key'],
+            consumer_secret = credentials['hits']['hits'][0]['_source']['consumer_secret'],
+            access_token_key = credentials['hits']['hits'][0]['_source']['access_token_key'],
+            access_token_secret = credentials['hits']['hits'][0]['_source']['access_token_secret'])
+            
+        print api.VerifyCredentials()
 
 #https://api.twitter.com/1/statuses/mentions.json?include_entities=true
 
 #https://dev.twitter.com/docs/auth/oauth
+
+x = TweetListen()
