377a8f0f0ae3ec37aee5c8f66823eedc68d1f3d8
Emanuil Tolev <emanuil.tolev@gmail.com>
2012-01-25 20:20:41 +0000

    Convert into server - flask does not seem suitable for this particular task, so while True: will do for now

--- whatid/tweetlisten.py
+++ whatid/tweetlisten.py
@@ -10,70 +10,80 @@ import whatid.dao
 import whatid.identifier
 
 class TweetListen(object):
+    api = None
     homeurl = 'http://localhost:5001/'
+    regexstr = '@emanuil_twitdev (.+)'
     def __init__(self):
         credentials = whatid.dao.TwitterCredentials.query(q='*')
         
-        api = twitter.Api(
+        self.api = twitter.Api(
         consumer_key = credentials['hits']['hits'][0]['_source']['consumer_key'],
         consumer_secret = credentials['hits']['hits'][0]['_source']['consumer_secret'],
         access_token_key = credentials['hits']['hits'][0]['_source']['access_token_key'],
         access_token_secret = credentials['hits']['hits'][0]['_source']['access_token_secret']
         )
+    
+    def listen(self):
+        regex = re.compile(self.regexstr, re.IGNORECASE)
+        i = 0 # debugging only
+        
+        while True:
+            mentions = self.api.GetMentions()
+            
+            for status in mentions:
+                i += 1
+                match = regex.search(status.text)
                 
-        regex = re.compile("@emanuil_twitdev (.+)", re.IGNORECASE)
-        i = 0
-        
-        mentions = api.GetMentions()
-        status = mentions[0]
-        
-        i += 1
-        match = regex.search(status.text)
-        if match:
-            q = match.group(1)
-            print q
-            # check the storage of identifiers, if already there, respond. else find it.
-            identifier = whatid.dao.Identifier.query(q=q)
-            if identifier['hits']['total'] != 0:
-                api.PostUpdate('@' + status.user.screen_name + ' ' + self.homeurl + 'identifier/' + q, in_reply_to_status_id = status.id)
-                print str(i)+' Got it! From the storage.'
-                # continue
-                exit()
+                if match:
+                    q = match.group(1)
+                    print q
+                    # check the storage of identifiers, if already there, respond. else find it.
+                    identifier = whatid.dao.Identifier.query(q=q)
+                    if identifier['hits']['total'] != 0:
+                        self.api.PostUpdate('@' + status.user.screen_name + ' ' + self.homeurl + 'identifier/' + q, in_reply_to_status_id = status.id)
+                        
+                        print str(i)+ ' ' + status.text + ' Got it! From the storage.'
+                        continue
 
-            ident = whatid.identifier.Identificator()
-            answer = ident.identify(q)
-            if answer:
-                # save the identifier with its type, and add to the success rate of the test
-                result = answer[0]
-                #obj = whatid.dao.Test.get(answer[0]['id'])
-                #obj['matches'] = obj.get('matches',0) + 1
-                #obj.save()
-                result['identifier'] = q
-                whatid.dao.Identifier.upsert(result)
-                
-                tweetreply = '@' + status.user.screen_name + ' '
-                
-                if result['url_prefix']:
-                    tweetreply += result['url_prefix']
-                    tweetreply += q
-                    # We can't have a URL Suffix ONLY, can we?
-                    if result['url_suffix']:
-                        tweetreply += result['url_suffix']
-                
-                tweetreply += ' ' + self.homeurl + 'identifier/' + q
-                
-                api.PostUpdate(tweetreply, in_reply_to_status_id = status.id)
-                print str(i)+' Got it! Engine ident.'
-                # continue
-                exit()
-            else:
-                whatid.dao.Identifier.upsert({"type":"unknown","identifier":q})
-                api.PostUpdate('@' + status.user.screen_name + ' Unknown identifier.', in_reply_to_status_id = status.id)
-                print str(i)+' Unknown identifier.'
-                # continue
-                exit()
-        else:
-            print 'No matches'
-        
+                    ident = whatid.identifier.Identificator()
+                    answer = ident.identify(q)
+                    if answer:
+                        # save the identifier with its type, and add to the success rate of the test
+                        result = answer[0]
+                        #obj = whatid.dao.Test.get(answer[0]['id'])
+                        #obj['matches'] = obj.get('matches',0) + 1
+                        #obj.save()
+                        result['identifier'] = q
+                        whatid.dao.Identifier.upsert(result)
+                        
+                        tweetreply = '@' + status.user.screen_name + ' '
+                        
+                        if result['url_prefix']:
+                            tweetreply += result['url_prefix']
+                            tweetreply += q
+                            # We can't have a URL Suffix ONLY, can we?
+                            if result['url_suffix']:
+                                tweetreply += result['url_suffix']
+                            tweetreply += ' '
+                        
+                        tweetreply += self.homeurl + 'identifier/' + q
+                        
+                        self.api.PostUpdate(tweetreply, in_reply_to_status_id = status.id)
+                        
+                        print str(i)+ ' ' + status.text + ' Got it! Engine ident.'
+                        continue
+                        
+                    else:
+                        whatid.dao.Identifier.upsert({"type":"unknown","identifier":q})
+                        self.api.PostUpdate('@' + status.user.screen_name + ' Unknown identifier.', in_reply_to_status_id = status.id)
+                        
+                        print str(i)+ ' ' + status.text + ' Unknown identifier.'
+                        continue
+                        
+                else:
+                    print str(i)+ ' ' + status.text + 'No matches.'
+            
+            sleep(60) # seconds
 
 x = TweetListen()
+x.listen()
